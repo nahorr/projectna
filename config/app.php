0063668c670a20b3835be60fd587a59cfa3f3e49
nahorr <nahorr@gmail.com>
2017-11-26 11:32:50 -0700

    notification

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'nahorrAnalytics'),
+    'name' => env('APP_NAME', 'Nahorr Analytics'),
 
     /*
     |--------------------------------------------------------------------------
