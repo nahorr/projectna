9275283883ed2b8e88cd956de71b13a190cc99a8
nahorr <nahorr@gmail.com>
2017-12-09 12:50:28 -0700

    products and services

--- resources/views/products-services.blade.php
+++ resources/views/products-services.blade.php
@@ -16,71 +16,85 @@
 				</div>
 			</div>
 			<!-- banner end -->
-
-
-
-			<!-- page-top start-->
+			<br>
+			<h1 class="text-center title">Core Services</h1>
+			<div class="separator"></div>
+			<!-- section start -->
 			<!-- ================ -->
-			<div class="page-top object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="100">
+			<div class="section clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
 				<div class="container">
+					<br>
 					<div class="row">
-						<div class="col-md-12">
-							<h1 class="text-center title">Core Services</h1>
-							<div class="separator"></div>
-							<p class="text-center">Our core services are list below:</p>
-							<div class="row grid-space-20">
-								<div class="col-sm-6">
-									<div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
-										<div class="icon-container default-bg">
-											<i class="fa fa-bold"></i>
-										</div>
-										<div class="body">
-											<h2>Data Collection & Analysis</h2>
-											<p>We provide data collection, analysis, and visualization services.</p>
-											<a href="{{url('/')}}" class="link"><span>Read More</span></a>
-										</div>
-									</div>
-									<div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
-										<div class="icon-container default-bg">
-											<i class="fa fa-check"></i>
-										</div>
-										<div class="body">
-											<h2>Software Development</h2>
-											<p>We provide custom software solutions.</p>
-											<a href="page-services.html" class="link"><span>Read More</span></a>
-										</div>
-									</div>
-									
-								</div>
-								<div class="col-sm-6">
-									<div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
-										<div class="icon-container default-bg">
-											<i class="fa fa-leaf"></i>
-										</div>
-										<div class="body">
-											<h2>Data Driven Website Design</h2>
-											<p>We provide custome web development solutions for businesses.</p>
-											<a href="page-services.html" class="link"><span>Read More</span></a>
-										</div>
-									</div>
-									<div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
-										<div class="icon-container default-bg">
-											<i class="fa fa-laptop"></i>
-										</div>
-										<div class="body">
-											<h2>Training and Talent Development</h2>
-											<p>we provide skills training and talent development services.</p>
-											<a href="page-services.html" class="link"><span>Read More</span></a>
-										</div>
-									</div>
-									
-								</div>
-							</div>
+						<div class="col-md-6">
+							<img src="{{asset('idea/images/nahorr-services-1.png')}}" alt="">
+							<div class="space hidden-md hidden-lg"></div>
+						</div>
+						<div class="col-md-6">
+							<p class="space-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi veniam, veritatis sint consequuntur! Natus, culpa, explicabo! Non quo laboriosam nobis quia, amet excepturi optio dolor impedit. Perferendis libero unde, nulla deserunt sequi. Voluptatum, officia suscipit porro reprehenderit vitae quo rem reiciendis, a vero quae. Vero beatae voluptatum, illo quaerat provident doloribus consequuntur saepe, id quas animi vel ad eligendi adipisci.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perspiciatis neque hic officia ullam quis quam molestias, tempora dicta exercitationem pariatur, perferendis ipsa laudantium rerum quasi debitis voluptas possimus, vitae.</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<!-- page-top end -->
+			<!-- section end -->
+			<hr>
+
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+				<div class="container">
+					<br>
+					<div class="row">
+						<div class="col-md-6">
+							<p class="space-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi veniam, veritatis sint consequuntur! Natus, culpa, explicabo! Non quo laboriosam nobis quia, amet excepturi optio dolor impedit. Perferendis libero unde, nulla deserunt sequi. Voluptatum, officia suscipit porro reprehenderit vitae quo rem reiciendis, a vero quae. Vero beatae voluptatum, illo quaerat provident doloribus consequuntur saepe, id quas animi vel ad eligendi adipisci.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perspiciatis neque hic officia ullam quis quam molestias, tempora dicta exercitationem pariatur, perferendis ipsa laudantium rerum quasi debitis voluptas possimus, vitae.</p>
+						</div>
+						<div class="col-md-6">
+							<img src="images/section-image-3.png" alt="">
+							<div class="space hidden-md hidden-lg"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
+			<hr>
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+				<div class="container">
+					<br>
+					<div class="row">
+						<div class="col-md-6">
+							<img src="images/section-image-2.png" alt="">
+							<div class="space hidden-md hidden-lg"></div>
+						</div>
+						<div class="col-md-6">
+							<p class="space-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi veniam, veritatis sint consequuntur! Natus, culpa, explicabo! Non quo laboriosam nobis quia, amet excepturi optio dolor impedit. Perferendis libero unde, nulla deserunt sequi. Voluptatum, officia suscipit porro reprehenderit vitae quo rem reiciendis, a vero quae. Vero beatae voluptatum, illo quaerat provident doloribus consequuntur saepe, id quas animi vel ad eligendi adipisci.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perspiciatis neque hic officia ullam quis quam molestias, tempora dicta exercitationem pariatur, perferendis ipsa laudantium rerum quasi debitis voluptas possimus, vitae.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
+			<hr>
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+				<div class="container">
+					<br>
+					<div class="row">
+						<div class="col-md-6">
+							<p class="space-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi veniam, veritatis sint consequuntur! Natus, culpa, explicabo! Non quo laboriosam nobis quia, amet excepturi optio dolor impedit. Perferendis libero unde, nulla deserunt sequi. Voluptatum, officia suscipit porro reprehenderit vitae quo rem reiciendis, a vero quae. Vero beatae voluptatum, illo quaerat provident doloribus consequuntur saepe, id quas animi vel ad eligendi adipisci.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit perspiciatis neque hic officia ullam quis quam molestias, tempora dicta exercitationem pariatur, perferendis ipsa laudantium rerum quasi debitis voluptas possimus, vitae.</p>
+						</div>
+						<div class="col-md-6">
+							<img src="images/section-image-1.png" alt="">
+							<div class="space hidden-md hidden-lg"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
 
 			<!-- page-top start-->
 			<!-- ================ -->
